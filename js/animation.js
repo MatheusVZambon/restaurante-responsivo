67c22133c0ea57b0120a21646e34a3ca6497dcdd
MatheusVZambon <matheusviebon@gmail.com>
2023-03-08 12:28:28 -0300

    Página Inicial Completa

---
+++ js/animation.js
@@ -0,0 +1,11 @@
+const InterObs = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add('slide-show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el => InterObs.observe(el)));
